1c42e7c877c1d3ae89528f81b74b85e14fad0f2a
noritakasawada <snp.inc.info@gmail.com>
2023-04-28 16:21:24 +0900

    c1

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,38 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import {useState, useEffect} from "react"
+
+function MyApp({ Component, pageProps }) {
+  const [liffObject, setLiffObject] = useState(null);
+  const [liffError, setLiffError] = useState(null);
+
+  useEffect(() => {
+    // to avoid `window is not defined` error
+    import("@line/liff").then((liff) => {
+      const liffInstance = liff.init ? liff : liff.default;
+      console.log("start liff.init()...");
+      liffInstance
+        .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID })
+        .then(() => {
+          console.log("liff.init() done");
+          setLiffObject(liffInstance);
+        })
+        .catch((error) => {
+          console.log(`liff.init() failed: ${error}`);
+          if (!process.env.NEXT_PUBLIC_LIFF_ID) {
+            console.info(
+              "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
+            );
+          }
+          setLiffError(error.toString());
+        });
+    });
+  }, []);
+
+  return (
+    <Component {...pageProps} />
+  )
 }
+
+export default MyApp
+
